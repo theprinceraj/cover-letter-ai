cc8a9275393cd6f10aa147941ab717dc6328a2c7
Prince Raj <profile.princeraj@gmail.com>
2025-07-05 23:13:30 +0530

    feat(app): configure global environment variables loading from .env file in AppModule

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,8 +3,17 @@ import { ConfigModule } from '@nestjs/config';
 import { EvalModule } from './eval/eval.module';
 import { DbModule } from './db/db.module';
 import { AuthModule } from './auth/auth.module';
+import { join } from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DbModule, AuthModule, EvalModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: join(__dirname, '../../../.env'),
+    }),
+    DbModule,
+    AuthModule,
+    EvalModule,
+  ],
 })
 export class AppModule {}
