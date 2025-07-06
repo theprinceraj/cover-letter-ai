091c14e2fdc0f248a0b5fb3e5ba448c43ee27932
Prince Raj <profile.princeraj@gmail.com>
2025-07-07 01:35:05 +0530

    feat(deps): add @nestjs/throttler package to manage request throttling

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,12 +4,22 @@ import { EvalModule } from './eval/eval.module';
 import { DbModule } from './db/db.module';
 import { AuthModule } from './auth/auth.module';
 import { join } from 'path';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: join(__dirname, '../../../.env'),
+    }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
+      errorMessage: 'Too many requests, please try again later.',
     }),
     DbModule,
     AuthModule,
